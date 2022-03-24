05774f08b2d8fcc9d1b5771ceb8228dd164cae9c
Imanolasolo <jjusturi@gmail.com>
2022-03-24 15:22:29 -0500

    132-array_to_heap.c

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -9,12 +9,11 @@
 
 heap_t *array_to_heap(int *array, size_t size)
 {
-	size_t i;
+	unsigned int i;
 	heap_t *root = NULL;
 
 	for (i = 0; i < size; i++)
-	{
 		heap_insert(&root, array[i]);
-	}
+
 	return (root);
 }
